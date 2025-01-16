44d2e9de56b37fa167319fb95360cc1ea17b8b12
Frantic-Dan <39160593+Frantic-Dan@users.noreply.github.com>
2025-01-17 01:45:00 +0800

    Create WeaponCameraFollow.cs

---
+++ Mods/0-KFCommonUtilityLib/KFAttached/Misc/WeaponCameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[AddComponentMenu("")]
+public class WeaponCameraFollow : MonoBehaviour
+{
+    public RenderTexture targetTexture;
+#if NotEditor
+    public ActionModuleDynamicSensitivity.DynamicSensitivityData dynamicSensitivityData;
+#endif
+
+    private void OnEnable()
+    {
+#if NotEditor
+        OcclusionManager.Instance.SetMultipleCameras(true);
+        if (dynamicSensitivityData != null)
+        {
+            dynamicSensitivityData.activated = true;
+        }
+#endif
+    }
+
+    private void OnDisable()
+    {
+#if NotEditor
+        OcclusionManager.Instance.SetMultipleCameras(false);
+        if (dynamicSensitivityData != null)
+        {
+            dynamicSensitivityData.activated = false;
+        }
+#endif
+        if (!targetTexture || !targetTexture.IsCreated())
+        {
+            return;
+        }
+        var cmd = new CommandBuffer();
+        cmd.SetRenderTarget(targetTexture);
+        cmd.ClearRenderTarget(true, true, Color.black);
+        Graphics.ExecuteCommandBuffer(cmd);
+        cmd.Dispose();
+    }
+}
